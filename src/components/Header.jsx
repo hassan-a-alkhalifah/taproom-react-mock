7ec6747b025082b116066e12c43e59e0c9008afa
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-09 11:51:58 -0800

    import Link, wrap both Home and Admin p tags with Link tags

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return(
     <div>
-    <p>HOME</p>
+    <Link to="/"><p>HOME</p></Link>
     <img alt="main logo"/>
-    <p>ADMIN</p>
+    <Link to="/admin"><p>ADMIN</p></Link>
     </div>
   );
 }

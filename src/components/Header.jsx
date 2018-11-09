a04750e87855ee838c31c3725c22f4252502b9e9
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-09 11:29:28 -0800

    add home and admin nav links, add main logo img tag with no image set

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <div>
+    <p>HOME</p>
+    <img alt="main logo"/>
+    <p>ADMIN</p>
+    </div>
+  );
+}
+
+export default Header;

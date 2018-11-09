9e937dfb4f1981e420749cfb9abf828776185ec7
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-09 11:50:50 -0800

    import KegsList and Admin components, add route tags for kegslist and admin components and wrap with Switch component tag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import KegsList from './KegsList';
+import Admin from './Admin';
 
 function App(){
   return (
     <div>
       <Header/>
+      <Switch>
+        <Route exact path='/' component={KegsList} />
+        <Route exact path='/admin' component={Admin} />
+      </Switch>
     </div>
   );
 }

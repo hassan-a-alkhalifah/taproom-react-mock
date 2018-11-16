866117434630978d180a8439acab76a0745caa93
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-11-16 09:24:16 -0800

    add ref variables for each tap spec, add handleNewTapFormSubmission function to handle taking inputs and providing to App component onNewTapCreation method as arguments, add ref prop to each input, define propTypes, import PropTypes

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Admin() {
+function Admin(props) {
+  let _beerBrandImage = null;
+  let _beerName = null;
+  let _beerPrice = null;
+  let _beerABV = null;
+  let _beerBrand = null;
+  let _pintLevel = null;
+
+  function handleNewTapFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTapCreation({beerBrandImage: _beerBrandImage.value, beerName: _beerName.value, beerPrice: _beerPrice.value, beerABV: _beerABV.value, beerBrand: _beerBrand.value, pintLevel: _pintLevel.value});
+    _beerBrandImage.value = '';
+    _beerName.value = '';
+    _beerPrice.value = '';
+    _beerABV.value = '';
+    _beerBrand.value = '';
+    _pintLevel.value = 0;
+  }
 
   const adminStyles = {
     width: '100%',
@@ -11,24 +29,19 @@ function Admin() {
     alignItems: 'center',
     marginTop: '112px',
     background: 'linear-gradient(to right, rgba(255,255,255,1) 0%, rgba(246,246,246,1) 47%, rgba(237,237,237,1) 100%)'
-  }
+  };
   const formStyles = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
     marginTop: '20px'
-  }
+  };
   const addButtonStyles = {
     padding: '10px',
     borderRadius: '4px',
     border: 'none',
     marginLeft: '137px'
-  }
-
-  const fileSelectedHandler = event => {
-    console.log("here");
-    console.log(event);
-  }
+  };
 
   return(
     <div style={adminStyles}>
@@ -56,35 +69,65 @@ function Admin() {
           }
       `}</style>
       <h1>Add New Beer</h1>
-      <form style={formStyles}>
+      <form onSubmit={handleNewTapFormSubmission} style={formStyles}>
         <div>
           <label for='beer-brand-image'>Beer Brand Image</label>
-          <input type='file' id='beer-brand-image'/>
+          <input
+             type='file'
+             id='beer-brand-image'
+             ref={(input) => {_beerBrandImage = input;}}
+          />
         </div>
         <div>
-          <label for='beer-brand-image'>Beer Name</label>
-          <input type='text' id='beer-name'/>
+          <label for='beer-name'>Beer Name</label>
+          <input
+            type='text'
+            id='beer-name'
+            ref={(input) => {_beerName = input;}}
+          />
         </div>
         <div>
           <label for='beer-price'>Beer Price</label>
-          <input type='text' id='beer-price'/>
+          <input
+            type='text'
+            id='beer-price'
+            ref={(input) => {_beerPrice = input;}}
+          />
         </div>
         <div>
-          <label for='beer-abv'>ABV</label>
-          <input type='text' id='beer-abv'/>
+          <label for='beer-ABV'>ABV</label>
+          <input
+            type='text'
+            id='beer-ABV'
+            ref={(input) => {_beerABV = input;}}
+          />
         </div>
         <div>
           <label for='beer-brand'>Beer Brand</label>
-          <input type='text' id='beer-brand'/>
+          <input
+            type='text'
+            id='beer-brand'
+            ref={(input) => {_beerBrand = input;}}
+          />
         </div>
         <div>
           <label for='pint-level'>Pint Level</label>
-          <input type='number' id='pint-level' min='0' max='100'/>
+          <input
+            type='number'
+            id='pint-level'
+            min='0'
+            max='100'
+            ref={(input) => {_pintLevel = input;}}
+          />
         </div>
         <button style={addButtonStyles} type='submit'>Add</button>
       </form>
     </div>
   );
+}
+
+Admin.propTypes = {
+  onNewTapCreation: PropTypes.func
 }
 
 export default Admin;
